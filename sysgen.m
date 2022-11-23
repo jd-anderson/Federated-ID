857c9dd5b2d389bf9509524e8c4fa1ee2abef1fe
LeoToso <108812632+LeoToso@users.noreply.github.com>
2022-11-23 11:07:09 -0400

    Update sysgen.m

--- sysgen.m
+++ sysgen.m
@@ -1,9 +1,9 @@
-function [A,B] = sysgen(A_0,B_0,V,U,M,delta)
+function [A,B] = sysgen(A_0,B_0,V,U,M,epsilon)
 
 A={};
 B={};
 for i=1:M(end)
-    A{i}=A_0 + delta*rand(1)*V;
-    B{i}=B_0 + delta*rand(1)*U;
+    A{i}=A_0 + epsilon*rand(1)*V;
+    B{i}=B_0 + epsilon*rand(1)*U;
 end 
 end
